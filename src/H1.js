e1e7e0c1bb3d7b23f9c35afc5194f6ebb237af2f
Sergeevich <mrssn84@mail.ru>
2023-03-10 18:20:55 +0400

    new project

---
+++ src/H1.js
@@ -0,0 +1,10 @@
+
+function H1() {
+  return (
+    <h1>
+      Привет мир!
+    </h1>
+  );
+}
+
+export default H1;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import H1 from './H1'
 import reportWebVitals from './reportWebVitals';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <p> Привет Я могу отобразить новости</p>
+      </div>
+    );
+  }
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <p className='header'>Просто код</p>
+    <H1 />
   </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
